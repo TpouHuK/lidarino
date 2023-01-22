1f7b8ae47ac8756e012ff387f71d2ba356562c42
tpouhuk <35433062+TpouHuK@users.noreply.github.com>
2023-01-22 19:00:53 +0300

    cleanup

--- src/bin/lidarino_cli.rs
+++ src/bin/lidarino_cli.rs
@@ -4,37 +4,6 @@ use std::ops::Range;
 use lidarino::motor::*;
 use lidarino::distance::*;
 use lidarino::mpu::*;
-
-fn wait_enter() {
-    use std::io::stdin;
-    let mut _s = String::new();
-    stdin().read_line(&mut _s).expect("Did not ented a string? wtf");
-    println!("Bye!...");
-}
-
-fn test_motors() {
-    let pitch_motor = StepMotor::new([4, 17, 27, 22]);
-    let yaw_motor = StepMotor::new([10, 9, 11, 5]);
-
-    let pitch_control = StepMotorController::new(pitch_motor, 3);
-    let yaw_control = StepMotorController::new(yaw_motor, 3);
-    yaw_control.stop();
-
-    loop {
-       pitch_control.set_pos(-300);
-        yaw_control.set_pos(-400);
-
-        pitch_control.wait_stop();
-        yaw_control.wait_stop();
-
-        pitch_control.set_pos(400);
-        yaw_control.set_pos(300);
-
-        pitch_control.wait_stop();
-        yaw_control.wait_stop();
-    }
-}
-
 use warp::Filter;
 
 fn sensors() -> String {
@@ -42,7 +11,7 @@ fn sensors() -> String {
 
     let data = sensor.read_distance();
     if let Ok((dist, qual)) = data {
-        format!("{{{}, {}}}", dist, qual)
+        format!("{{{dist}, {qual}}}")
     } else {
         "Error".to_string()
     }
