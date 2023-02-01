2da066ac2d110a82439f93bf829a8a00219b11e4
tpouhuk <35433062+TpouHuK@users.noreply.github.com>
2023-02-01 17:45:27 +0300

    Add test python scripts

---
+++ misc/test_evenly_points.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 100
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+x = []
+y = []
+def add_point(point):
+    x.append(x)
+    y.append(y)
+
+
+x = np.random.rand(N)
+y = np.random.rand(N)
+
+plt.scatter(x, y)
+plt.show()
