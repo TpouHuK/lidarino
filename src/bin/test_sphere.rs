7de0c1129f7605951807733587d67f51bbe03b73
tpouhuk <35433062+TpouHuK@users.noreply.github.com>
2023-02-04 21:43:01 +0300

    wip

--- src/bin/test_sphere.rs
+++ src/bin/test_sphere.rs
@@ -5,18 +5,18 @@ fn main() {
     let opts = ScanOptions {
         amount_of_points: 100,
         pitch_start: 0.0,
-        pitch_end: 120.0,
+        pitch_end: 180.0,
         yaw_start: 180.0 - 90.0,
         yaw_end: 180.0 + 90.0,
     };
     let points = lidarino::sphere::generate_points(opts);
-    for p in &points {
-        println!("{} {} {}", p.x, p.y, p.z);
-    }
+    //for p in &points {
+    //    println!("{} {} {}", p.x, p.y, p.z);
+    //}
     let waypoints: Vec<Waypoint> = points.into_iter().map(|p| p.into()).collect();
     let waypoints = optimize_path(waypoints, Duration::from_secs(1));
     for waypoint in waypoints {
-        //println!("{} {}", waypoint.yaw, waypoint.pitch);
+       println!("{} {}", waypoint.yaw, waypoint.pitch);
     }
     //let waypoints = nearest_neightbour_solve(waypoints);
 }
