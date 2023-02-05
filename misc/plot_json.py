a52eb493850a0ab27a25fc646c17d350679f82b1
tpouhuk <35433062+TpouHuK@users.noreply.github.com>
2023-02-05 11:52:18 +0300

    wip

--- misc/plot_json.py
+++ misc/plot_json.py
@@ -1,6 +1,8 @@
 from sys import stdin
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import json
+import random
 
 def reshape_points(points):
     list_x = []
@@ -15,17 +17,20 @@ def reshape_points(points):
     return (list_x, list_y, list_z)
 
 data = json.load(open("points.json"))
+
 points = []
+colors = []
 
 for p in data:
     points.append((p["x"], p["y"], p["z"]))
+    colors.append(p["quality"])
 
 x, y, z = reshape_points(points)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.set_aspect('equal', adjustable='box')
-ax.scatter(x, y, z)
+ax.scatter(x, y, z, c=colors, cmap = mpl.colormaps["plasma"])
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
--- src/hardware/motor.rs
+++ src/hardware/motor.rs
@@ -196,8 +196,8 @@ impl ControllerSharedData {
         self.step_delay_ms.store(step_delay_ms, Ordering::Relaxed);
     }
 
-    fn get_step_delay_ms(&self) {
-        self.step_delay_ms.load(Ordering::Relaxed);
+    fn get_step_delay_ms(&self) -> u32 {
+        self.step_delay_ms.load(Ordering::Relaxed)
     }
 
     fn set_current_pos(&self, current_pos: i32) {
@@ -357,8 +357,8 @@ impl StepMotorController {
     }
 
     /// Get delay between motor steps.
-    pub fn get_step_delay_ms(&self, step_delay_ms: u32) {
-        self.shared.get_step_delay_ms();
+    pub fn get_step_delay_ms(&self) -> u32 {
+        self.shared.get_step_delay_ms()
     }
 
     /// Checks if motor is running or not.
